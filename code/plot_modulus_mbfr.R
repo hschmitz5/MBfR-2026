faa18415e5da81c88b8802021053f8ff413cf5f9
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-07-15 11:40:18 -0500

    added AGS

--- code/plot_modulus_mbfr.R
+++ code/plot_modulus_mbfr.R
@@ -5,11 +5,11 @@ library(patchwork)
 source("./code/01_load_ps.R")
 
 # define sample names
-region <- c("inner", "outer")
+biofilm_levels <- c("inner", "outer", "XS")
 
 fname_in <- "./data/RheometryApr142026.xlsx"
 modulus <- read_excel(fname_in, sheet = "input", skip = 1) %>%
-  select(region, freq_rad, G_avg, G_sd, G2_avg, G2_sd) %>%
+  select(biofilm, freq_rad, G_avg, G_sd, G2_avg, G2_sd) %>%
   pivot_longer(
     cols = c(G_avg, G_sd, G2_avg, G2_sd),
     names_to = c("measure", ".value"),
@@ -20,9 +20,11 @@ modulus <- read_excel(fname_in, sheet = "input", skip = 1) %>%
     avg = avg/1e3, 
     sd = sd/1e3,
     # change display names and order
+    biofilm = factor(biofilm, levels = biofilm_levels),
     measure = factor(measure, levels = c("G", "G2")),
     measure = recode(measure,"G"="Storage Modulus","G2"="Loss Modulus")
-    )
+    ) %>%
+  filter(freq_rad < 110)
 
 modulus_subset <- modulus %>%
   filter(freq_rad == 0.1) %>%
@@ -30,17 +32,17 @@ modulus_subset <- modulus %>%
 
 #### Plot
 
-p <- ggplot(modulus, aes(x = freq_rad, y = avg, color = region)) +
+p <- ggplot(modulus, aes(x = freq_rad, y = avg, color = biofilm)) +
   geom_point() +
-  geom_line(aes(group = region)) +
+  geom_line(aes(group = biofilm)) +
   geom_errorbar(
     aes(ymin = pmax(avg - sd, 0), ymax = avg + sd),
     width = 0.2
   ) +
   facet_wrap(~measure, scales = "free_y", nrow = 1) +
   scale_color_manual(
-    name = "Region",
-    values = c("inner" = "steelblue", "outer" = "lightsalmon2")
+    name = "Biofilm",
+    values = c("inner" = "steelblue", "outer" = "lightsalmon2", "XS" = "black")
   ) +
   labs(
     x = "Frequency (rad/s)",
@@ -57,7 +59,7 @@ p <- ggplot(modulus, aes(x = freq_rad, y = avg, color = region)) +
 fname_out <- "./figures/moduli.png"
 ggsave(fname_out, plot = p, width = 6.5, height = 2.25, dpi = 300)
 
-p_sub <- ggplot(modulus_subset, aes(x = region, y = avg, fill = measure)) +
+p_sub <- ggplot(modulus_subset, aes(x = biofilm, y = avg, fill = measure)) +
   geom_col(position = "dodge", width = 0.6) +
   geom_errorbar(
     aes(ymin = avg - sd, ymax = avg + sd),
@@ -66,7 +68,7 @@ p_sub <- ggplot(modulus_subset, aes(x = region, y = avg, fill = measure)) +
   ) +
   labs(
     title = "Frequency = 0.1 rad/s",
-    x = "Region",
+    x = "Biofilm",
     y = "Modulus (kPa)"
   ) +
   scale_fill_manual(
