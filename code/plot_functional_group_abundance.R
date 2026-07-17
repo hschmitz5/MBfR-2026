392350a6d8b72af1714df83961e6fff3dc6e9bbb
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-07-17 15:18:09 -0500

    first commit

---
+++ code/plot_functional_group_abundance.R
@@ -0,0 +1,75 @@
+# NOTE: Using all data, not just top n_show
+# This is to verify that major metabolic groups are not being dropped
+
+rm(list = ls())
+source("./code/01_load_ps.R")
+source("./code/02_sum_rel_ab_by_function.R")
+
+write2excel <- 0
+
+metab_order <- c("Methanogen", "Filamentous")
+n_rows <- length(metab_order)
+
+rel_ab_df <- sum_rel_ab_by_function(ps) %>%
+  filter(metab %in% metab_order) %>%
+  mutate(
+    metab = factor(metab, levels = metab_order)
+  )
+
+# ------------ Plot ------------------
+
+
+p <- ggplot(rel_ab_df, aes(x = Sample, y = mean_sum, fill = metab_val)) +
+  geom_col(position = "dodge", width = 0.6) +
+  geom_errorbar(
+    aes(ymin = mean_sum - sd_sum, ymax = mean_sum + sd_sum),
+    width = 0.2,
+    position = position_dodge(width = 0.6)
+  ) +
+  facet_wrap(~metab, scales = "free_y") +
+  labs(
+    y = "Relative\nAbundance (%)",
+    x = "Size"
+  ) +
+  scale_fill_manual(
+    name = "Functional Group",
+    values = c("Positive" = "steelblue",
+               "Positive + Variable" = "lightgray")
+  ) +
+  theme_classic(base_size = 12) +
+  theme(
+    # plot.title = element_text(hjust = 0.5), # center title
+    # legend.position = "bottom",
+    strip.background = element_rect(
+      colour = NA # facet label outline
+    )
+  ) 
+  
+# Save plot
+fname <- "./figures/functional_group_abundance.png"
+ggsave(fname, plot = p, width = 6.5, height = 2, dpi = 300)
+
+
+
+# ------ Write Data to Excel
+
+if (write2excel == 1) {
+  ### Do not exclude data
+  # define relative abundance
+  rel_wide <- get_rel_wide(ps) %>%
+    rownames_to_column(var = "Genus")
+  
+  new_m <- get_metabolism(rel_wide) %>%
+    # true if any metabolic groups in row are defined
+    mutate(tf = as.integer(if_any(everything(), ~ !is.na(.x)))) %>%
+    rownames_to_column(var = "Genus")
+  
+  full_df <- left_join(rel_wide, new_m, by = "Genus") %>%
+    filter(tf == 1) %>%
+    dplyr::select(-tf) %>%
+    relocate(where(is.numeric), .after = where(is.character)) %>%
+    arrange(Genus)
+  
+  library(writexl)
+  write_xlsx(full_df, path = "./data/functional_rel_ab.xlsx")
+}
