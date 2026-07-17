43a968014cb237a59943107b4ff2d479c567bde5
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-07-17 10:47:17 -0500

    update_labels

--- code/plot_eps_mbfr.R
+++ code/plot_eps_mbfr.R
@@ -16,7 +16,7 @@ group_data <- function(fname) {
       .groups = "drop"
     ) %>%
     mutate(
-      region = factor(region, levels = c("inner", "outer")),
+      region = factor(region, levels = c("Inner", "Outer")),
       extract = recode(extract,"LB" = "Loosely Bound","TB" = "Tightly Bound"),
       extract = factor(extract, levels = c("Tightly Bound", "Loosely Bound"))
     )
@@ -44,20 +44,21 @@ df_conc <- bind_rows(
   'Total EPS (PN + PS)' = df_wide %>% select(extract, region, avg = total, sd),
   .id = "assay"
   ) %>%
-  mutate(plot_type = "\u00b5g/mgTSS") %>%
-  select(plot_type, assay, extract, region, avg, sd) 
+  mutate(y_label = "\u00b5g/mgTSS") %>%
+  select(y_label, assay, extract, region, avg, sd) 
 
 # Calculate PN/PS
 PNPS <- df_wide %>% 
   mutate(
-    plot_type = "PN/PS",
+    y_label = "",
     assay = "PN/PS",
     sd = NA
   ) %>%
-  select(plot_type, assay, extract, region, avg = PNPS, sd) 
+  select(y_label, assay, extract, region, avg = PNPS, sd) 
   
 df_all <- bind_rows(df_conc, PNPS) %>%
   mutate(
+    y_label = factor(y_label, levels = c("\u00b5g/mgTSS", "")),
     assay = factor(assay, levels = c("Polysaccharide (PS)", "Protein (PN)", "Total EPS (PN + PS)", "PN/PS"))
     )
 
@@ -68,12 +69,12 @@ p <- ggplot(df_all, aes(x = region, y = avg, fill = assay)) +
   
   # Concentration Plots
   geom_col(
-    data = subset(df_all, plot_type == "\u00b5g/mgTSS"),
+    data = subset(df_all, y_label == "\u00b5g/mgTSS"),
     position = "dodge",
     width = 0.8
   ) +
   geom_errorbar(
-    data = subset(df_all, plot_type == "\u00b5g/mgTSS"),
+    data = subset(df_all, y_label == "\u00b5g/mgTSS"),
     aes(ymin = avg - sd, ymax = avg + sd),
     position = position_dodge(width = 0.8),
     width = 0.2
@@ -81,13 +82,13 @@ p <- ggplot(df_all, aes(x = region, y = avg, fill = assay)) +
   
   # PN/PS plots
   geom_col(
-    data = subset(df_all, plot_type == "PN/PS"),
+    data = subset(df_all, y_label == ""),
     width = 0.5
   ) +
   
   # Sizes
   facet_grid(
-    plot_type ~ extract,
+    y_label ~ extract,
     scales = "free_y",
     switch = "y"
   ) +
