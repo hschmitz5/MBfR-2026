07eb38bd171569dcee76c0ba64ca4841ef9d2d4f
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-08-06 15:19:54 -0500

    remove na.rm flag

--- code/02_sum_rel_ab_by_function_ags.R
+++ code/02_sum_rel_ab_by_function_ags.R
@@ -29,7 +29,7 @@ sum_rel_ab_by_function_ags <- function(ps) {
         filter(Genus %in% taxa_list[[nm]]) %>%
         group_by(size.name, Sample) %>%
         summarize(
-          sum_abund = sum(Abundance, na.rm = TRUE),
+          sum_abund = sum(Abundance),
           .groups = "drop"
         ) %>%
         mutate(metab = nm)
@@ -45,8 +45,8 @@ sum_rel_ab_by_function_ags <- function(ps) {
     df %>%
       group_by(metab, size.name) %>%
       summarize(
-        mean_sum = mean(sum_abund, na.rm = TRUE),
-        sd_sum = sd(sum_abund, na.rm = TRUE),
+        mean_sum = mean(sum_abund),
+        sd_sum = sd(sum_abund),
         .groups = "drop"
       ) %>%
       mutate(metab_val = value_col)
@@ -59,7 +59,7 @@ sum_rel_ab_by_function_ags <- function(ps) {
   full_summary <- bind_rows(P_summary, PV_summary) %>%
     # define panel grouping
     mutate(
-      size.name = factor(size.name, levels = size$name)
+      size.name = factor(size.name, levels = size_meta$name)
     ) 
 
 }
