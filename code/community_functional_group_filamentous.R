76fbed785eb5ce32474f0b421fdd7965c759fd36
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-08-10 15:13:39 -0500

    rename figure

--- code/community_functional_group_filamentous.R
+++ code/community_functional_group_filamentous.R
@@ -65,7 +65,7 @@ p <- ggplot(rel_ab_df, aes(x = exp_category, y = mean_sum)) + #, fill = metab_va
   ) 
   
 # Save plot
-fname <- "./figures/functional_group_filamentous.tif"
+fname <- "./figures/Figure_4.tif"
 ggsave(fname, plot = p, width = 6.5, height = 2.5, dpi = 300)
 
 
